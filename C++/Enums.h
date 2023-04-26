f71b95fe87b954ffcdc7e8ca8a0f08c3403dda88
y.kim <kim.yaroslavik@gmail.com>
2023-04-26 18:02:24 +0300

    Copy work repo

---
+++ C++/Enums.h
@@ -0,0 +1,24 @@
+//
+// Created by justsomedude on 25.03.23.
+//
+namespace Stella {
+    enum VisitableTag {
+        tagTypePlaceholder,
+
+        tagTypeBool,
+        tagTypeNat,
+        tagTypeUnit,
+        tagTypeTuple,
+
+        tagTypeSumType,
+
+        tagTypeFunction,
+        tagTypeIdent,
+
+        tagTypePanic,
+
+        tagTypeRecord,
+
+        tagTypeRef
+    };
+}
